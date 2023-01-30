c85f92e03a02afec5b94842d3492941c93700f1f
suneelkumargavara <sunilkumargavara@gmail.com>
2023-01-31 00:15:21 +0530

    1. adds config to load env variables 2. adds setup file to delete db

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -27,4 +27,20 @@ describe('Authentication System (e2e)', () => {
         expect(email).toEqual('mamr@mamr.com');
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+    expect(body.email).toEqual(email);
+  });
 });
